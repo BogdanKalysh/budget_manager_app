9f70aff314dc5a88245ed05a66f373d7ff0f521f
Andriy Pasemko <pa.pasemko.andriy@gmail.com>
2021-05-07 19:23:25 +0300

    __

--- budget_manager_backend/main.cpp
+++ budget_manager_backend/main.cpp
@@ -1,10 +1,7 @@
 #include <QCoreApplication>
-#include "querybuilders.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QSqlDatabase tst = setUpDatabase();
-    qDebug() << tst.databaseName();
     return a.exec();
 }
