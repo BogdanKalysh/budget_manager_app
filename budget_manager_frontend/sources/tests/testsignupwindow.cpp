c7ce6aca3a9ccecd1ed29a24226b3541e902ba95
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2021-06-07 20:59:16 +0300

    Add files via upload

--- budget_manager_frontend/sources/tests/testsignupwindow.cpp
+++ budget_manager_frontend/sources/tests/testsignupwindow.cpp
@@ -10,36 +10,36 @@ void TestSignUpWindow::testSignUp()
     signUpWindow.setName("");
     signUpWindow.setEmail("");
     signUpWindow.setPassword("");
-    signUpWindow.setName("");
+    signUpWindow.setPasswordConfirmation("");
     QCOMPARE(signUpWindow.on_signUpButton_clicked(),1);
     //Tests for passwords do not match
     signUpWindow.setName("Username");
     signUpWindow.setEmail("username@gmail.com");
     signUpWindow.setPassword("password");
-    signUpWindow.setName("passpord");
+    signUpWindow.setPasswordConfirmation("passpord");
     QCOMPARE(signUpWindow.on_signUpButton_clicked(),1);
     //Tests for wrong password
     signUpWindow.setName("Username");
     signUpWindow.setEmail("username@gmail.com");
     signUpWindow.setPassword("passwo");
-    signUpWindow.setName("passwo");
+    signUpWindow.setPasswordConfirmation("passwo");
     QCOMPARE(signUpWindow.on_signUpButton_clicked(),3);
     //Tests for wrong password
     signUpWindow.setName("Username");
     signUpWindow.setEmail("username@gmail.com");
     signUpWindow.setPassword("passwo");
-    signUpWindow.setName("passwo");
+    signUpWindow.setPasswordConfirmation("passwo");
     QCOMPARE(signUpWindow.on_signUpButton_clicked(),3);
     //Tests for wrong email
     signUpWindow.setName("Username");
     signUpWindow.setEmail("usernamegmail.com");
     signUpWindow.setPassword("password");
-    signUpWindow.setName("password");
+    signUpWindow.setPasswordConfirmation("password");
     QCOMPARE(signUpWindow.on_signUpButton_clicked(),3);
     //Test for all correct
     signUpWindow.setName("Username");
     signUpWindow.setEmail("username@gmail.com");
     signUpWindow.setPassword("password");
-    signUpWindow.setName("password");
+    signUpWindow.setPasswordConfirmation("password");
     QCOMPARE(signUpWindow.on_signUpButton_clicked(),0);
 }
